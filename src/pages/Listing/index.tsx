f9ec3c52ba73df95b54f5bc77073cf3c6acf4c02
Nicola <lanzoni.nicola@gmail.com>
2022-01-11 11:48:45 -0300

    Listing page template

--- src/pages/Listing/index.tsx
+++ src/pages/Listing/index.tsx
@@ -1,5 +1,26 @@
+import MovieCard from "components/MovieCard/MovieCard";
 import Pagination from "../../components/Pagination";
 
 export default function Listing() {
-  return <Pagination />;
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
